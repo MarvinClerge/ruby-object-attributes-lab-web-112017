146ba088e27ce79153977426f519ba55284773c2
Marvin Clerge <clergemarvin@gmail.com>
2017-11-21 14:43:25 -0500

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  #names
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  #breeds
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  #names
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  #job
+  def job=(title)
+    @job = title
+  end
+  def job
+    @job
+  end
+end
